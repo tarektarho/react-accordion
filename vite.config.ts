5e0e144411414b3f4a8e9d1532e13d26344603d1
Tarek Tarho <tarek_tarho@hotmail.com>
2023-08-20 23:15:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true,
+  },
   build: {
     outDir: "build",
     sourcemap: true,
